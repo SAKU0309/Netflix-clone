2fadcd3ef725e7fdc30a075453083ed180ee2c74
Sakshi <sakshichy14@gmail.com>
2025-03-02 22:49:11 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.PNG', '**/*.webp']
+  assetsInclude: ['**/*.png', '**/*.PNG', '**/*.webp'],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"], // Separate React into its own chunk
+          vendor: ["lodash", "axios"], // Split other dependencies
+        },
+      },
+    },
+  },
 })
 
 
